d69122be753171010d738f957f81d654e43e9eb2
Martin Lowe <martin.lowe@eclipse-foundation.org>
2021-06-08 13:06:04 -0400

    Update external organizations endpoint for caching

--- src/main/java/org/eclipsefoundation/react/request/OrganizationResource.java
+++ src/main/java/org/eclipsefoundation/react/request/OrganizationResource.java
@@ -11,15 +11,26 @@
  */
 package org.eclipsefoundation.react.request;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.eclipsefoundation.react.api.OrganizationAPI;
+import org.eclipsefoundation.react.api.model.Organization;
+import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Allows for external organizations data to be retrieved and displayed. This
@@ -28,7 +39,8 @@ import org.eclipsefoundation.react.api.OrganizationAPI;
  */
 @Path("organizations")
 @Produces(MediaType.APPLICATION_JSON)
-public class OrganizationResource {
+public class OrganizationResource extends AbstractRESTResource {
+    public static final Logger LOGGER = LoggerFactory.getLogger(OrganizationResource.class);
 
     @RestClient
     @Inject
@@ -36,6 +48,34 @@ public class OrganizationResource {
 
     @GET
     public Response get() {
-        return Response.ok(orgAPI.organizations("", 1)).build();
+        Optional<List<Organization>> orgs = cache.get("all", new MultivaluedMapImpl<>(), Organization.class, () -> getAll(null));
+        if (orgs.isEmpty()) {
+            return Response.noContent().build();
+        }
+        return Response.ok(orgs.get()).build();
+    }
+
+    @GET
+    @Path("{orgID}")
+    public Response get(@PathParam("orgID") String organizationID) {
+        Organization org = orgAPI.organizationByID(organizationID);
+        if (org == null) {
+            return Response.noContent().build();
+        }
+        return Response.ok(orgAPI.organizationByID(organizationID)).build();
+    }
+
+    private List<Organization> getAll(String workingGroup) {
+        String actualWG = workingGroup == null ? "" : workingGroup;
+        List<Organization> orgs = new LinkedList<>();
+        Set<Organization> tmp = Collections.emptySet();
+        int count = 1;
+        do {
+            tmp = orgAPI.organizations(actualWG, count);
+            orgs.addAll(tmp);
+            LOGGER.error("{}",tmp);
+            count++;
+        } while(!tmp.isEmpty() && tmp != null);
+        return orgs;
     }
 }
